b4faf737a84fbea1828299733e0492bdbcc5f348
CristianoSellit <99354780+CristianoSellitto@users.noreply.github.com>
2022-03-29 17:16:55 +0000

    Added program.

--- main.cs
+++ main.cs
@@ -1,7 +1,26 @@
+// Cristiano
+// ICS2O-Unit3-01-CSharp
+// March 29 2022
+
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int sidea;
+        int sideb;
+        int height;
+        int output;
+        
+        Console.WriteLine("Enter side A (cm):");
+        sidea = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter side B (cm):");
+        sideb = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter height (cm):");
+        height = Convert.ToInt32(Console.ReadLine());
+        output = (((sidea + sideb) / 2) * height);
+        Console.WriteLine("\nThe area of this trapezoid is " + (output) + "cm².");
+        Console.WriteLine("\nFinished.");
+    }
 }
